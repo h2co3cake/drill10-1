168d40bd354718e164300d51f44f4c947b92ae31
Leejuwon <bbit456@naver.com>
2018-10-11 20:51:32 +0900

    drill10-1_2
    타이틀화면 출력

--- start_state.py
+++ start_state.py
@@ -1,6 +1,8 @@
 import game_framework
 from pico2d import *
 
+import title_state
+
 
 name = "StartState"
 image = None
@@ -21,7 +23,7 @@ def update():
     global logo_time
     if (logo_time > 1.0):
         logo_time = 0
-        game_framework.quit()
+        game_framework.change_state(title_state)
     delay(0.01)
     logo_time += 0.01
 
