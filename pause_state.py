b63c04080cdd561c453e95f7cc988de9412aafe5
Leejuwon <bbit456@naver.com>
2018-10-11 21:31:06 +0900

    drill10-1  완성
    2017180031 이주원
    commit: 7번
    https://youtu.be/E5XKLYYwaRU

--- pause_state.py
+++ pause_state.py
@@ -19,11 +19,14 @@ def exit():
 def handle_events():
     events = get_events()
     for event in events:
-        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-            game_framework.pop_state(main_state)
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.pop_state()
 
 
 def draw():
+    global image
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
